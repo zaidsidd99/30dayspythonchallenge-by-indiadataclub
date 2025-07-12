644bc1dc7c16c734afcea974bc81ad8cbb22d0fa
zaidsidd99 <zaidabu858@gmail.com>
2025-07-12 14:34:49 +0530

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,71 @@
+
+# zaid_expense = [1200,3000,4000,10000]
+# zack_expense = [3000,4000,2500,1000]
+
+# total=0
+# for item in zaid_expense:
+#     total=total+item
+# print ("total of zaid_expense:",total)
+
+
+# total=0
+# for item in zack_expense:
+#     total=total+item
+# print ("total of zack_expense:",total)
+
+# ##################################################################
+
+# def calculate_total (exp):
+#     total=0
+#     for item in exp:
+#         total=total+item
+#     return total
+    
+
+
+# zaid_expense = [1200,3000,4000,10000]
+# zack_expense = [3000,4000,2500,1000]
+
+# zaid_total=calculate_total(zaid_expense)
+# zack_total=calculate_total(zack_expense)
+
+# print("zaid total expense:",zaid_total)
+# print("zack total expense:",zack_total)
+
+# #########################################################
+
+# def sum (a,b):
+#     total=a+b
+#     return total
+
+# n=sum (5,8)
+# print ("total sum:",n)
+
+###########################################################
+
+
+
+
+
+
+
+
+# Write a function that computes the sum and average of a list of numbers ?
+
+def total_sum (a,b,c,d):
+    total= a+b+c+d
+    return total
+
+tom=total_sum(2,4,6,8)
+print("total sum of zaid:",tom)
+
+
+
+
+def average (a,b,c,d):
+    total= a+b+c+d 
+    average= total / 4
+    return average
+
+mike= average(2,4,6,8)
+print("average of mike:",mike)
